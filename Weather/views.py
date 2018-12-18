3561ba46717451085d776648daaf9fdc98cb43c1
_rahul_._ai <35560689+bera5186@users.noreply.github.com>
2018-12-18 13:47:00 +0530

    Update views.py

--- Weather/views.py
+++ Weather/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, HttpResponse
 import requests
-from geopy.geocoders import Nominatim
-from .map import createMap
+
 
 # Create your views here.
 def index(request):
@@ -9,12 +8,7 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=a5ec053718588afe7c9be31031631cc6'
     city = 'delhi'
     
-    """ The Localtion Map Stuff """
-    geolocator = Nominatim(user_agent="Weather", timeout=3)
-    location = geolocator.geocode(city)
     
-    """ Creating map.html """
-    createMap(location.latitude, location.longitude, city)
 
     response = requests.get(url.format(city)).json()
 
@@ -22,9 +16,7 @@ def index(request):
             'city': city,
             'temperature' : response['main']['temp'],
             'description' : response['weather'][0]['description'],
-            'icon' : response['weather'][0]['icon'],
-            'lat' : location.latitude,
-            'long' : location.longitude,
+            'icon' : response['weather'][0]['icon']
         }
 
     ref = {'city_weather' : city_weather}
@@ -33,12 +25,7 @@ def index(request):
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=a5ec053718588afe7c9be31031631cc6'
         city = request.POST.get('city')
 
-        """ The Localtion Map Stuff """
-        geolocator = Nominatim(user_agent="Weather", timeout=3)
-        location = geolocator.geocode(city)
-
-        """ Creating map.html """
-        createMap(location.latitude, location.longitude, city)
+    
 
 
         response = requests.get(url.format(city)).json()
@@ -47,9 +34,7 @@ def index(request):
             'city': city,
             'temperature' : response['main']['temp'],
             'description' : response['weather'][0]['description'],
-            'icon' : response['weather'][0]['icon'],
-            'lat' : location.latitude,
-            'long' : location.longitude,
+            'icon' : response['weather'][0]['icon']
         }
 
         ref = {'city_weather' : city_weather}
